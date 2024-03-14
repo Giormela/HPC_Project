5e56782656f95191c960575bcef6b13afe4b9e50
Giormela <ciociogiorgio@gmail.com>
2024-03-14 17:59:26 +0100

    Debug

--- ACO.py
+++ ACO.py
@@ -1,7 +1,7 @@
 from lib.colony import *
 from lib.path import *
 
-colony = Colony(N=10, rho=0.1, delta=0.1)
+colony = Colony(N=20, rho=0.1, delta=0.1)
 
-colony.simulate(iter=20)
+colony.simulate(iter=10)
 
