b8758c311f8d16616105bfbf5b576f0ffd0dead4
wwolfi <73550881+wwolfi@users.noreply.github.com>
2024-03-15 15:01:21 +0100

    added launcher bash script

--- ACO.py
+++ ACO.py
@@ -1,5 +1,9 @@
+import subprocess
 from lib.colony import *
 from lib.path import *
+
+# Load modules on HPC system
+subprocess.call(['bash', './run_subprocess.sh'])
 
 colony = Colony(N=20, rho=0.1, delta=0.1)
 
---
+++ run_subprocess.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "=============================="
+
+source /etc/profile 
+
+module load py-mpi4py/3.1.4/gcc-12.3.0-openmpi intel-oneapi-compilers/2023.1.0/gcc-11.4.0
+
