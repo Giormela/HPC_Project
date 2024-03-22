47fe3b84a9bc7d6b41921793d8a9d3761d0ff4d2
Alex MELHEM <alex.melhem@student-cs.fr>
2024-03-22 15:50:32 +0100

    Comments for the launcher code

--- ACO-mpi-scatter.py
+++ ACO-mpi-scatter.py
@@ -9,11 +9,12 @@ from libmpi.param import PARAMS_DICT
 from libmpi.redistribution import RedistributionStrategy
 from libmpi.path import set_user_id
 
+# Create MPI environment
 comm = MPI.COMM_WORLD
 Me = comm.Get_rank()
 size = comm.Get_size()
-print("Me: ", Me, "Size: ", size)
 
+# Define the shell arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-u','--user',type=int, choices=range(1, 15))
 parser.add_argument('-m','--method',type=int, choices=range(1, 4))
@@ -23,6 +24,8 @@ parser.add_argument('-d','--delta',type=float)
 parser.add_argument('-r','--rho',type=float)
 parser.add_argument('-min',type=float)
 parser.add_argument('-max',type=float)
+
+# Parse the shell arguments
 args = parser.parse_args()
 user_id = int(args.user) if args.user else 1
 method = args.method if args.method else 1
@@ -33,12 +36,10 @@ rho = args.rho if args.rho else 0.1
 min = args.min if args.min else 0.
 max = args.max if args.max else float('inf')
 
-def get_array_from_solution(solution):
-  return np.array([PARAMS_DICT[param].get_index_from_value(solution[param]) for param in PARAMS_DICT])
+# Set the user id for all processes
+set_user_id(user_id)
 
-def get_solution_from_array(array):
-  return {param: PARAMS_DICT[param].get_value_by_index(array[i]) for i, param in enumerate(PARAMS_DICT)}
-
+# initialize the colony on the root process
 if Me == 0:
   # Choose the redistribution strategy
   if method == 1:
@@ -47,30 +48,31 @@ if Me == 0:
     redistribution_strategy = RedistributionStrategy.Quadratic
   else:
     redistribution_strategy = RedistributionStrategy.Relu
-  # Set and sharing user id 
 
+  # Create the colony
   if min != 0 or max != np.inf:
     colony = ColonyMinMax(min=min, max=max, rho=rho, delta=delta, N=N, redistribution_strategy=redistribution_strategy)
   else:
     colony = Colony(rho=rho, delta=delta, N=N, redistribution_strategy=redistribution_strategy)
 
-set_user_id(user_id)
+# Run the ACO algorithm
 for i in range(ITER):
   
   solutions = None
+  
   if Me == 0:
     print(f"Iteration {i}")
-    print("time accumulated", colony.execution_time)
     solutions = colony.run()
     solutions = [[solutions[j] for j in range(i, len(solutions), size)] for i in range(size)]
   
   batch = comm.scatter(solutions, root=0)
   
   results = []
+  
   for s, solution in batch:
     results.append((s, cost_function(solution["olevel"], solution["simd"], "256", "256", "256", solution["num_threads"], "100", solution["n1_size"], solution["n2_size"], solution["n3_size"])))
   
-  results = comm.gather(results, root=0)
+  results += comm.gather(results, root=0)
   
   if Me == 0:
     results = [item for sublist in results for item in sublist]
