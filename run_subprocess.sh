fb3fd05c861df7e2c7ea3677b074b810dbeec2a5
wwolfi <73550881+wwolfi@users.noreply.github.com>
2024-03-27 09:53:09 +0100

    added valgrind import

--- run_subprocess.sh
+++ run_subprocess.sh
@@ -4,5 +4,5 @@ echo "=============================="
 
 source /etc/profile 
 
-module load py-mpi4py/3.1.4/gcc-12.3.0-openmpi intel-oneapi-compilers/2023.1.0/gcc-11.4.0
+module load py-mpi4py/3.1.4/gcc-12.3.0-openmpi intel-oneapi-compilers/2023.1.0/gcc-11.4.0 valgrind/3.20.0/gcc-12.3.0-openmpi
 
