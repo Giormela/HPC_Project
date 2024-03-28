e8005dcfb81816636d2e7b06d95360daf0b2ac8b
Giormela <ciociogiorgio@gmail.com>
2024-03-28 12:19:54 +0100

    Renaming

--- __main__.py
+++ __main__.py
@@ -1,13 +1,12 @@
 import os
 import argparse
 
-numberProcess=-1
 parser = argparse.ArgumentParser(
                     prog='Iso3d Ant Optimization',
                     description='We run an ant colony to find the best paramters for an iso3d executions',
                     epilog='Good Luck :)')
-parser.add_argument('-np','--numberOfProc',type=int, choices={1,2,3,4,5,6,7,8}, help="Number of processes instantiated from MPI")
-parser.add_argument('-m','--method',type=str, choices={'linear','relu','hyperbolic','performance'},help="Pheromons distribution method linear, relu or quadratic")
+parser.add_argument('-np','--numberOfProc',type=int, choices={1,2,3,4,5,6,7,8}, help="number of processes created by MPI")
+parser.add_argument('-m','--method',type=str, choices={'linear','relu','hyperbolic','performance'},help="pheromons distribution method linear, relu or quadratic")
 parser.add_argument('-i','--iteration',type=int, help="number of iterations the ant colony algorithm")
 parser.add_argument('-n','--numberOfAnt',type=int)
 parser.add_argument('-s','--size',type=int, choices={256, 512}, help="Problem size")
@@ -17,7 +16,6 @@ parser.add_argument('--min',type=float,help="the minimum pheromon each edge can 
 parser.add_argument('--max',type=float,help="the maximum pheromon each edge can hold")
 parser.add_argument('--cachegrind', nargs='?', const=True, default=False, help="adds the test of valgrind with cachegrind for the best result at the end")
 
-# Parse the shell arguments
 args = parser.parse_args()
 np = args.numberOfProc if args.numberOfProc else 1
 method = args.method if args.method else 'linear'
@@ -35,5 +33,5 @@ if args.cachegrind:
 
 cmd = "/usr/bin/mpirun -np "+str(np)+" python3 ACO-mpi-scatter.py --method "+method+" --iter "+str(iter)+" --numberOfAnt "+str(n)+" --delta "+str(delta)+" --rho "+str(rho)+" --min "+str(min)+" --max "+str(max)+" "+cachegrid_flag
 
-os.system("./run_subprocess.sh")
+os.system("./load_module.sh")
 os.system(cmd)
